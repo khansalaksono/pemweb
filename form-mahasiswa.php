11668ebce30c4937c73250e1ca2ebd95e708c06c
khansalaksono <khansalaksono@gmail.com>
2023-04-12 03:52:30 +0700

    revision to NAMA from NIM

--- form-mahasiswa.php
+++ form-mahasiswa.php
@@ -1,7 +1,7 @@
 <form method="POST" action="insert-mahasiswa.php">
     <label>NIM</label>
     <input type=text id=nim name=nim />
-    <label>NIM</label>
+    <label>NAMA</label>
     <input type=text id=nama name=nama />
-    <input type=submit /> 
+    <input type=submit />
 </form>
