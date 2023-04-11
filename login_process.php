3fbce9f71db9291b04ea89974393c50c0382a242
khansalaksono <khansalaksono@gmail.com>
2023-04-12 04:07:03 +0700

    coba define username password

--- login_process.php
+++ login_process.php
@@ -2,15 +2,15 @@
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-if(empty($username) || empty($password)){
-    echo "Username dan password harus diisi";
-} else{
-    if($username == "admin" && $password == "tes123"){
-        echo "Login sukses";
-    }
-    else{
-        echo "Login gagal";
-    } 
-}
+// $username = "admin";
+// $password = "tes123";
 
-?>
+if (empty($username) || empty($password)) {
+    echo "Username dan password harus diisi";
+} else {
+    if ($username == "admin" && $password == "tes123") {
+        echo "Login sukses";
+    } else {
+        echo "Login gagal";
+    }
+}
