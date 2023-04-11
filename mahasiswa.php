4a3090f4bbd3bec3c268abd9cdf5554ba3dc2b09
khansalaksono <khansalaksono@gmail.com>
2023-04-12 04:58:23 +0700

    try using table for the output in mahasiswa.php

--- mahasiswa.php
+++ mahasiswa.php
@@ -2,23 +2,35 @@
 <br />
 <br />
 
-<?php
-require_once 'config.php';
+<table style="border-collapse: collapse; width:fit-content">
+    <thead>
+        <tr>
+            <th style="padding:10px; border:1px solid black;">NIM</th>
+            <th style="padding:10px; border:1px solid black;">Nama</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        require_once 'config.php';
 
-$sql = "select * from mahasiswa";
+        $sql = "select * from mahasiswa";
 
-// Pakai db::fetchAll
-$query = $db->query($sql);
-$rows = $query->fetchAll();
-foreach ($rows as $row) {
-    echo $row["nim"] . " " . $row["nama"] . "<br/>";
-}
+        // Pakai db::fetchAll
+        $query = $db->query($sql);
+        $rows = $query->fetchAll();
+        foreach ($rows as $row) {
+            echo "<td style='padding:10px; border:1px solid black'>{$row["nim"]}";
+            echo "<td style='padding:10px; border:1px solid black'>{$row["nama"]}";
+            //echo $row["nim"] . " " . $row["nama"] . "<br/>";
+        }
 
-/*
-//Pakai db::query
-$query = $db->query($sql);
-foreach($query as $row){
-    echo $row["nim"]." ".$row["nama"]."<br/>";
-}*/
+        /*
+        //Pakai db::query
+        $query = $db->query($sql);
+        foreach($query as $row){
+            echo $row["nim"]." ".$row["nama"]."<br/>";
+        }*/
 
-?>
+        ?>
+    </tbody>
+</table>
